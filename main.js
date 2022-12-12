46ad157a243eb6358c9d6170770905e547648309
vero_romanescu <romanescuveronica278@gmail.com>
2022-12-12 16:04:57 +0200

    Final project

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 function generateButtons() {
+    document.getElementById('mainButton').style.visibility = 'hidden';
     let userInput = document.querySelector("#userInput");
     var random = Math.floor(Math.random() * userInput.value);
     let counter = 1;
-    for (let i = 0; i < userInput.value; ++i) {
-    const btn = document.createElement("button");
-    btn.onclick = function() {
-        if (i == random && counter <= 3) {
-            alert('You did it 🤩🤩🤩🤩🤩'); 
-            ++counter;
-            return false;
-        } else if (counter < 3 && i != random) {
-            alert('Try again! You have ' + (3 - counter) + ' chances left!');
-            ++counter;
-            return false;
-        } else {
-            alert("You lost 💀");
+    for (let i = 1; i <= userInput.value; ++i) {
+        const btn = document.createElement("button");
+        btn.onclick = function() {
+            if (i == random - 1 && counter <= 3) {
+                alert('You did it'); 
+                ++counter;
+                return false;
+            } else if (counter < 3 && i != random - 1) {
+                alert('Try again! You have ' + (3 - counter) + ' chances left!');
+                ++counter;
+                return false;
+            } else {
+                alert.className = "alert alert-succes";
+                alert("You lost");
+                window.location.reload();
+            }
         }
+        btn.innerHTML = "Player " + i;
+        document.body.appendChild(btn);
+        btn.className = "btn btn-warning";
     }
-    btn.innerHTML = "Player " + i;;
-    document.body.appendChild(btn);
-    btn.style.backgroundColor = "#F4A460";
-    }
-} 
-// you have three chances to guess the button. If you did't do it, you lost the game;
-// You'll be told how many chances you are left with;
+}
