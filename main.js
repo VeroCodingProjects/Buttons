ba41222c3bb45ca492756067cd2abe979c80b1de
vero_romanescu <romanescuveronica278@gmail.com>
2022-12-20 15:12:57 +0200

    User frendly :)

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
 function generateButtons() {
     document.getElementById('mainButton').style.visibility = 'hidden';
     let userInput = document.querySelector("#userInput");
-    if (userInput.value > 100) {
+    let winnerIndex = Math.floor(Math.random() * userInput.value);
+    if (userInput.value < 2 || userInput.value > 999) {
         window.location.reload();
-        alert("The input must be a number under 100 and over 1");
         return 0;
     }
-    let winnerIndex = Math.floor(Math.random() * userInput.value);
-    let leftChances = 2;
+    let chancesLeft = 2;
     for (let i = 0; i < userInput.value; ++i) {
         const btn = document.createElement("button");
         btn.onclick = function() {
+            document.body.removeChild(document.body.lastChild);
             if (i == winnerIndex) {
-                alert('You did it');
-                window.location.reload();
-            } else if (leftChances > 0 && i != winnerIndex) {
-                alert('Try again! You have ' + leftChances + ' chances left!');
-                --leftChances;
-            } else {
-                alert("You lost");
+                const winningMessage = document.createElement("h2");
+                winningMessage.innerHTML = "YOU DID IT!"
+                document.body.appendChild(winningMessage);
+            } else if (i != winnerIndex && chancesLeft >= 1) {
+                --chancesLeft;
+            } else if (chancesLeft == 0) {
+                alert("You lost!")
                 window.location.reload();
             }
         }
         btn.innerHTML = "Player " + (i + 1);
         document.body.appendChild(btn);
         btn.className = "btn btn-warning";
+    }
+}
+
+function chancesUpdate() {
+    const shapeRole = document.createElement("h3");
+    shapeRole.innerHTML = "Left chances"
+    shapeRole.style.align = "center";
+    document.body.appendChild(shapeRole);
+    for (let i = 1; i < 4; ++i) {
+        const chance = document.createElement("h3");
+        chance.className = "circle";
+        chance.style.align = "center";
+        document.body.appendChild(chance);
     }
 }
