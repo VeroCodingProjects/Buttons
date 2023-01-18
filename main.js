dcb544611064b61f764eda2976d5ff2545a24820
vero_romanescu <romanescuveronica278@gmail.com>
2023-01-18 13:43:18 +0200

    send input via form

--- main.js
+++ main.js
@@ -1,63 +1,41 @@
 let chancesLeft = 3;
 
-//getting the input and creating the buttons and the life circles
-function startGame() {
-    document.getElementsByTagName('button')[0].style.visibility = 'hidden';
-    let winnerIndex = Math.floor(Math.random() * document.getElementsByTagName('input')[0].value);
-    createButtons(document.getElementsByTagName('input')[0], winnerIndex);
-    createCircles();
+function startGame(data) {
+    data.button.style.visibility = 'hidden';
+    let winnerIndex = Math.floor(Math.random() * data.input.value);
+    createButtons(data.input.value, winnerIndex);
 }
 
-// creating the buttons
-function createButtons(userInput, winnerIndex) {
-    for (let i = 0; i < userInput.value; ++i) {
+function createButtons(input) {
+    for (let i = 0; i < input; ++i) {
         const btn = document.createElement("button");
-        btn.onclick = function() {
-            changingStatus(i, winnerIndex);
-        }
+        btn.onclick = changeStatus;
         btn.innerHTML = "Player " + (i + 1);
-        document.getElementsByTagName("div")[0].appendChild(btn);
+        document.getElementById("game-status").appendChild(btn);
         btn.className = "btn btn-warning";
     }
 }
-// creating the circles
-function createCircles() {
-    for (let i = 1; i < 4; ++i) {
-        const chance = document.createElement("h3");
-        chance.className = "circle";
-        chance.style.align = "center";
-        document.getElementsByTagName("div")[0].appendChild(chance);
-    }
-}
 
-// updating the status of the game
-function changingStatus(i, winnerIndex) {
-    document.getElementsByTagName("div")[0].removeChild(document.getElementsByTagName("div")[0].lastChild);
+function changeStatus(i, winnerIndex) {
     if (i == winnerIndex) {
-        document.getElementsByTagName("h1")[0].innerText = "YOU WON!\n"
-        document.getElementsByTagName("div")[0].style.visibility = 'hidden';
+        document.getElementById("game-result").innerText = "YOU WON!\n";
+        document.getElementById("game-status").style.visibility = 'hidden';
         resetTheGame();
     } else if (i != winnerIndex && chancesLeft > 1) {
         --chancesLeft;
     } else if (chancesLeft == 1) {
-        document.getElementsByTagName("h1")[0].innerText = "YOU LOST!\n"
-        document.getElementsByTagName("div")[0].style.visibility = 'hidden';
+        document.getElementById("game-result").innerText = "YOU LOST!\n";
+        document.getElementById("game-status").style.visibility = 'hidden';
         resetTheGame();
     }
 }
 
-//after the winning or the loss, the user will get the cance to restart the game
 function resetTheGame() {
     const resetButton = document.createElement("button");
     resetButton.innerHTML = "Reset the game"
-    document.getElementsByTagName("h1")[0].appendChild(resetButton);
+    document.getElementById("game-result").appendChild(resetButton);
     resetButton.className = "btn btn-warning";
     resetButton.onclick = function() {
-        refreshPage();
+        window.location = window.location; 
     }
-}
-
-//refreshes the game
-function refreshPage() {
-    window.location.reload();
 }
